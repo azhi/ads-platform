50a8aad8ac6bf29c66fa81b1b513feee3e20013f
azhi <astzhe@gmail.com>
2013-02-20 20:57:28 +0300

    quick fix: added admin user to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,9 +20,15 @@ users = []
   users << user
 end
 
+admin = User.new(nickname: "admin", email: "admin@example.com",
+                 password: "foobar12", password_confirmation: "foobar12")
+admin.role = :admin
+admin.skip_confirmation!
+admin.save
+
 ads = []
 ads << Advertisement.create(content: "Bla-bla-bla", type_id: types.first.id, user_id: users.first.id)
-ads << Advertisement.create(content: "Bla-bla-bla", type_id: types.second.id, user_id: users.first.id)
+ads << Advertisement.create(content: "Other bla", type_id: types.second.id, user_id: users.first.id)
 
 ads.first.pictures.create([{ url: "http://fc05.deviantart.net/fs71/i/2011/265/f/1/limbo__or_smth_like_that_by_shine_blue-d4akqph.jpg" },
                            { url: "http://www.scificool.com/images/2012/01/Will-Smth-and-Tommy-Lee-Jones-in-Men-in-Black-3-2012-Movie-Image.jpg" }])
